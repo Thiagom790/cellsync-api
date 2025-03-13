b95063d0f08591eb432e1232670ff72eddd1a3a9
Thiago Teixeira <thiagom790@gmail.com>
2025-03-13 14:49:26 -0300

    refactor: improve cell repository code

--- src/CellSync.Infrastructure/DataAccess/Repositories/CellRepository.cs
+++ src/CellSync.Infrastructure/DataAccess/Repositories/CellRepository.cs
@@ -6,27 +6,11 @@ namespace CellSync.Infrastructure.DataAccess.Repositories;
 
 internal class CellRepository(CellSyncDbContext dbContext) : ICellRepository
 {
-    public async Task AddAsync(Cell cell)
-    {
-        await dbContext.Cells.AddAsync(cell);
-    }
+    public async Task AddAsync(Cell cell) => await dbContext.Cells.AddAsync(cell);
 
-    public async Task<Cell?> GetByIdAsync(Guid cellId)
-    {
-        var result = await dbContext.Cells.FindAsync(cellId);
+    public async Task<Cell?> GetByIdAsync(Guid cellId) => await dbContext.Cells.FindAsync(cellId);
 
-        return result;
-    }
+    public async Task<List<Cell>> GetAllAsync() => await dbContext.Cells.AsNoTracking().ToListAsync();
 
-    public async Task<List<Cell>> GetAllAsync()
-    {
-        var result = await dbContext.Cells.AsNoTracking().ToListAsync();
-
-        return result;
-    }
-
-    public void Update(Cell cell)
-    {
-        dbContext.Cells.Update(cell);
-    }
+    public void Update(Cell cell) => dbContext.Cells.Update(cell);
 }
